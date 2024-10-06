2a693272bf90c8862bec8d121cfcc8bdbee20c4e
WasimSamirArsad <wasimakhtar12091@gmail.com>
2024-10-06 15:26:33 +0530

    structure of project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="shipment price prediction",
+    author="wasim akhtar",
+    author_email="wasimcode12091@gmail.com",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
